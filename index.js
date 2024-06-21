16ea1e0a6510b6c1f059f68e9260039d68d3d659
hlkgt <haihaioell@gmail.com>
2024-06-21 13:14:24 +0700

    optimizing be

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const corsOptions = {
   origin: "https://funquized-fe.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
 };
 
 app.use(cors(corsOptions));
