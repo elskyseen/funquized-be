d5f50a1ab0e88475358a6e112d584a6dff7dc6fd
hlkgt <haihaioell@gmail.com>
2024-06-21 21:11:24 +0700

    remove creating isLogin cookie

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -59,12 +59,6 @@ export const login = async (req, res) => {
         sameSite: "None",
         partitioned: true,
       });
-      res.cookie("isLogin", true, {
-        maxAge: 24 * 60 * 60 * 1000,
-        secure: true,
-        sameSite: "Strict",
-        partitioned: true,
-      });
       // update column refresh_token on database
       await prisma.users.update({
         where: {
